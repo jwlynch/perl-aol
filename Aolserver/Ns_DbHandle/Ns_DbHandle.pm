c8df6edc40f80be57da348907d35af5b711556ab
Jim Lynch <jwl@users.sourceforge.net>
2000-11-26 05:52:42 +0000

    Started adding docs; not yet complete.

--- Aolserver/Ns_DbHandle/Ns_DbHandle.pm
+++ Aolserver/Ns_DbHandle/Ns_DbHandle.pm
@@ -78,13 +78,292 @@ get placed into a special Ns_Set which is handled specially by the system.
 
 You can get exactly one row in a single statement which includes the SQL
 string, or at most one row. You can also issue a Select statement which
-includes SQL and then iterate over the incoming rows.
+includes SQL and then iterate over the matching rows.
 
 The methods:
 
 =over 4
 
+=item new()
+
+Overview
+
+Creates a new object which encapsulates Aolserver's Ns_DbHandle data structure.
+It gets the Ns_DbHandle from a pool of existing handles.
+
+SYNTAX
+
+  <scalar Lvalue> = new Aolserver::Ns_DbHandle ( <pool name string> );
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar
+                         (which will be the new handle object), and
+       <pool name string> is a string containing the name of the desired
+                          pool. Said pool must exist.
+
+DESCRIPTION
+
+The new() method allocates and constructs a perl object to hold a database
+connection handle. The handle returned can then be used to query and modify
+the database. The actual connection object is pulled from a pool of existing
+live connections to the database; when destroyed, it returns the connection
+to the pool.
+
+Upon success, you get a handle; if it fails, new() returns undef.
+
+It's constructed in such a way that when it goes out of scope, it's
+deallocated.
+
+=item BindRow()
+
+Overview
+
+Return an Ns_Set structure of column names to be returned by 
+the previously-executed (i.e., using Exec) SQL command 
+
+SYNTAX
+
+  <scalar Lvalue> =
+       <handle object> -> 
+                BindRow();
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar
+                         (which will be an Ns_Set maybe containing the row),
+       <handle object> is a reference to an Aolserver::Ns_DbHandle object.
+
+DESCRIPTION
+
+The BindRow method returns an Ns_Set structure whose key names are the 
+column names of rows to be returned by the SQL command previously-executed 
+by Exec. If the SQL command does not return rows (i.e., the Exec method 
+did not return NS_ROWS), NS_ERROR is returned.
+
+=item Cancel()
+
+Overview
+
+Cancel an active SQL select statement (which would have been initiated
+by the Select() method)
+
+SYNTAX
+
+  <scalar Lvalue> =
+       <handle object> -> 
+                Cancel();
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar 
+                         (which will be an error/success code),
+       <handle object> is a reference to an Aolserver::Ns_DbHandle object, and
+
+DESCRIPTION
+
+The Cancel method is similar to the Ns_DbFlush function, but instead of 
+allowing the select statement to complete and send all selected rows, 
+Cancel sends a cancels message to the database. This can result in 
+faster interruption of a long-running query. Cancel returns NS_OK 
+on success and NS_ERROR on error.
+
+=item Exec()
+
+Overview
+
+Execute any arbitrary SQL command 
+
+SYNTAX
+
+  <scalar LValue> = 
+       <handle object> -> 
+                Exec( <sql string> );
+
+Description
+
+The Exec method executes the specified SQL command on the specified 
+database connection. Exec returns one of the following status codes: 
+
+ NS_ERROR  if the SQL command fails
+ NS_DML    if the SQL command is DML (Data Manipulation Language) 
+              or DDL (Data Definition Language) 
+ NS_ROWS   if the SQL command will return rows (such as a SELECT command)
+
+This function allows you to write a true ad hoc query tool and process SQL 
+statements without knowing ahead of time if they return rows or are DDL or 
+DML statements.
+
+=item ExecDML() (note, calls Ns_DbDML())
+
+Overview
+
+Execute an SQL DML statement 
+
+Syntax
+
+  <scalar Lvalue> = <handle object> -> ExecDML( <sql string> );
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar 
+                         (which will be an error/success code),
+       <handle object> is a reference to an Aolserver::Ns_DbHandle object, and
+
+Description
+
+The ExecDML function executes SQL that should be a data manipulation 
+language statement such as an insert or update, or data definition 
+language such as a create table. If the statement was executed 
+successfully, ExecDML returns NS_OK. If the statement results in rows 
+being returned or a database error, ExecDML returns NS_ERROR. 
+
+Detailed error messages may have accumulated in an internal buffer in 
+the Ns_DbHandle.
+
+Examples
+
+[META: convert to perl]
+
+    my $handle = new Aolserver::Ns_DbHandle("aPoolName");
+    my $status;
+
+    if ($handle) 
+    {
+        $status = $handle->ExecDML
+                ("insert into aTable (colName1,colName2) values (1,2)");
+
+        if ($status != NS_OK) 
+        {
+                # handle error condition
+        }       
+    }
+
+=item Flush()
+
+Overview
+
+Flush any waiting rows 
+
+
+Syntax
+
+    int Ns_DbFlush(
+    Ns_DbHandle *handle
+    );
+
+Description
+
+The Flush method fetches and dumps any waiting rows after a call of the 
+Select() method. This function is useful when you have already fetched 
+all the rows you intend to process. Flush returns NS_OK after successfully 
+flushing the database or NS_ERROR on error.
+
+Flush is called automatically when Ns_DbHandle's are returned to their 
+pools with Aolserver::NsDbHandle's DESTROY method, which calls the
+Ns_DbPutHandle() function to make sure the handle is ready the next time 
+it is used.
+
+Some database drivers will also cancel any active transactions when 
+Flush() is called.
+
+=item GetRow
+
+Overview
+
+Fetch the next waiting row after an Ns_DbSelect 
+
+SYNTAX
+
+    <scalar Lvalue> = <handle object> -> GetRow( <scalar containing row> );
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar 
+                         (which will be an error/success code),
+       <handle object> is a reference to an Aolserver::Ns_DbHandle object, and
+       <scalar containing row> contains the row object that was output from
+                                 the Select() method
+
+Description
+
+The GetRow method fetches the next row waiting to be retrieved after 
+a Select(). The row Ns_Set must be the result of a previous Select. 
+GetRow frees any existing values of the set and sets the values to 
+the next row fetched from the database. Possible return values are: 
+
+ NS_OK       A row has been fetched and more rows may be waiting.
+ NS_END_DATA No row has been fetched and there are no more rows waiting.
+ NS_ERROR    A database error occurred, or the function has already 
+             returned NS_END_DATA but has been called again anyway.
+
+You cannot call ExecDML, GetOneRow, or GetOneRowAtMost with the same 
+database handle while fetching rows from the database in a GetRow 
+loop. Doing so flushes any waiting rows and a subsequent call to GetRow 
+will fail. You can do so if you use separate database handles.
+
+Examples
+
+[META: convert to perl]
+
+        Ns_DbHandle *handle;
+        Ns_Set *row;
+        int             status;
+        handle = Ns_DbPoolGetHandle("mypool");
+        row = Ns_DbSelect(handle, "select * from mytable");
+        if (row == NULL) {
+                /*... handle select error ...*/
+        }
+        while ((status = Ns_DbGetRow(handle, row)) == NS_OK) {
+                /*... process the row fetched from the database ...*/
+        }
+        if (status != NS_END_DATA) {
+                /*... handle get row error ...*/
+        }
+
+=item GetOneRow() (note, this calls Ns_Db1Row())
+
+Overview
+
+Execute an SQL statement that must return one row 
+
+SYNTAX
+
+  <scalar Lvalue> =
+       <handle object> -> 
+                GetOneRow( <sql string> );
+
+ where <scalar Lvalue> is a perl Lvalue that can receive a scalar 
+                         (which will be an Ns_Set maybe containing the row),
+       <handle object> is a reference to an Aolserver::Ns_DbHandle object, and
+       <sql string> is the SQL command presumed to produce 
+                       exactly one row.
+
+DESCRIPTION
+
+The GetOneRow method calls the Ns_Db0or1Row function with the given SQL 
+statement. If Ns_Db0or1Row returns 1 row, GetOneRow returns the newly 
+allocated Ns_Set for the row. Under normal conditions, the row set will
+be freed for you automatically. If NsDb0or1Row returns zero rows, GetOneRow 
+returns undef.
+
+If the SQL statement returns zero rows or a database error has occurred, 
+GetOneRow returns undef. Detailed error messages may have accumulated in 
+an internal buffer in the Ns_DbHandle.
+
+Examples
+
+    my $row;
+    my $handle = new Aolserver::Ns_DbHandle("aPoolName");
+    
+    if ($handle) 
+    {
+        $row = $handle->GetOneRow( "select aName from aTable" );
+
+        if ($row) 
+        {
+            my $value = $row->Get("aName");
+            # (use $value here)
+        }
+
+        $handle->DESTROY(); # this should not be necessary... for now it is tho
+    } 
+    
 =item GetOneRowAtMost() (note, this calls Ns_Db0or1Row())
+
+Overview
+
+Execute an SQL statement that must return at most 1 row 
 
 SYNTAX
 
@@ -125,23 +404,45 @@ an internal buffer in the Ns_DbHandle.
 
 Examples
 
-(meta: this is the orig C example; rewrite)
+    my $row;
+    my $nrows;
+    my $handle = new Aolserver::Ns_DbHandle("main");
+    if ($handle)
+    {
+     	$row =
+            $handle->GetOneRowAtMost("select aValue from aTable", $nrows);
 
-    Ns_Set *row;
-    int nrows;
-    Ns_DbHandle *handle;
-    if ((handle = Ns_DbPoolGetHandle("aPoolName")) != NULL) {
-        row = Ns_Db0or1Row(handle, "select aName from aTable",
-                                                        &nrows);
-        if (row != NULL && nrows == 1) {
-                char *value;
-                value = Ns_SetGet(row, "aName");
-                /* use `value' here */
-                Ns_SetFree(row);
-        }
-    } 
+	if ($row && $nrows == 1)
+	{
+            my $value;
+            $value = $row->Get("aValue");
+
+            # (use $value somehow here)
+            
+            # note, set in $row will be freed when $row goes out of scope
+	}
+
+        $handle->DESTROY(); # this should not be necessary... for now it is tho
+    }
+
+=item GetSelectRow
+
+=item InterpretSqlFile
+
+=item InSelectLoop
+
+=item Select
+
+=item SetSelectRow
+
+=item DESTROY
 
 =back
+
+=head1 BUGS
+
+perl won't dispose of the Ns_DbHandle. You have to call the DESTROY() method
+yourself; not doing so causes a handle to leak.
 
 =head1 AUTHOR
 
